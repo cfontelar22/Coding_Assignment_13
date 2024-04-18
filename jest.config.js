9e17a9e489091031ae23057317e2773ffcc6251a
cfontelar <fontelarcj@gmail.com>
2024-04-17 19:02:54 -0500

    Initial commit

--- jest.config.js
+++
@@ -1,9 +0,0 @@
-module.exports = {
-  testEnvironment: "jsdom",
-  // moduleNameMapper: {
-  //   ".(css|less|scss)$": "identity-obj-proxy",
-  // },
-  moduleNameMapper: {
-    '\\.(jpg|jpeg|png|gif|webp|svg)$': '<rootDir>/__mocks__/fileMock.js',
-  },
-};
